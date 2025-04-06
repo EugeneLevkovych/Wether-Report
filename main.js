940e7c54842c6cee39c829ca24c5aee4da9e510e
EugeneLevkovych <eugenelevkovych@yahoo.com>
2025-04-06 23:49:48 +0300

    add await

--- main.js
+++ main.js
@@ -19,35 +19,36 @@ const createWeatherTemplate = (weatherInfo) => {
   `;
 };
 
-const fetchWeatherData = (city) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-  fetch(url)
-    .then((responce) => {
+    const fetchWeatherData = async (city) => {
+    try {
+      const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
       if (!responce.ok) {
-        throw new Error(responce.status);
-      }
-      return responce.json();
-    })
-    .then((data) => {
-      const weatherTemplate = createWeatherTemplate(data);
-      weatherDisplayEl.innerHTML = weatherTemplate;
-      loaderEl.classList.add('is-hidden');
-      // document.getElementById('weather-input').value = city;
-    })
-    .catch((err) => {
-      errorDisplayEl.innerHTML = `<p class="error-text">${err}.City not found</p>`;
-      searchFormEl.reset();
-      weatherDisplayEl.innerHTML = "";
-      loaderEl.classList.add('is-hidden');
-    });
-};
+         throw new Error(responce.status);
+        }
+    
+        const data = await responce.json();
+        // console.log(data);
+        const weatherTemplate = createWeatherTemplate(data);
+
+        weatherDisplayEl.innerHTML = weatherTemplate;
+        loaderEl.classList.add('is-hidden');
+    }
+    catch(err) {
+        errorDisplayEl.innerHTML = `<p class="error-text">${err}.City not found</p>`;
+        searchFormEl.reset();
+        weatherDisplayEl.innerHTML = "";
+        loaderEl.classList.add('is-hidden');
+    }    
+}
+
+
 
 const onSearchFormSubmit = (event) => {
   event.preventDefault();
   loaderEl.classList.remove('is-hidden');
 
   const city = event.currentTarget.elements.weather_input.value.trim();
+  // console.log(city);
   if (city === "") {
     errorDisplayEl.innerHTML = '<p class="error-text">Please write the name of the city</p>'
     loaderEl.classList.add('is-hidden');
@@ -61,14 +62,13 @@ searchFormEl.addEventListener("submit", onSearchFormSubmit);
 
 document.addEventListener("DOMContentLoaded", fetchWeatherData("Kyiv"));
 
-// showLoader();
-//loaderEl.classList.remove('is-hidden');
-//loaderEl.classList.add('is-hidden');
+// ---------------------------then,catch---------------------------------------
 
-// ------------------------------------------------------------------
 // const searchFormEl = document.querySelector(".js-search-form");
 // const weatherDisplayEl = document.querySelector(".js-weather-display");
-// const errorDisplayEl =  document.querySelector('.js-error-display');
+// const errorDisplayEl = document.querySelector('.js-error-display');
+// const loaderEl = document.querySelector('.loader');
+// const apiKey = 'c8f40d3a3736e508fdb5b271f6f49dd4';
 
 // const createWeatherTemplate = (weatherInfo) => {
 //   return `
@@ -82,17 +82,10 @@ document.addEventListener("DOMContentLoaded", fetchWeatherData("Kyiv"));
 //      <p class="weather-display-text">Humidity: ${weatherInfo.main.humidity}%</p>
 //      <p class="weather-display-text">Wind Speed: ${weatherInfo.wind.speed} m/s</p>
 //     </div>
-//          `;
+//   `;
 // };
 
-// const onSearchFormSubmit = (event) => {
-//   event.preventDefault();
-
-//   const city = event.currentTarget.elements.weather_input.value.trim();
-//   if (city === "") {
-//     alert("Please write the name of the city");
-//     return;
-//   }
+// const fetchWeatherData = (city) => {
 //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //   fetch(url)
@@ -103,20 +96,32 @@ document.addEventListener("DOMContentLoaded", fetchWeatherData("Kyiv"));
 //       return responce.json();
 //     })
 //     .then((data) => {
-//         console.dir(data);
-        
 //       const weatherTemplate = createWeatherTemplate(data);
 //       weatherDisplayEl.innerHTML = weatherTemplate;
+//       loaderEl.classList.add('is-hidden');
 //     })
 //     .catch((err) => {
-//       errorDisplayEl.innerHTML = `<p style="color: red;">City not found</p>`;
-//         searchFormEl.reset();
+//       errorDisplayEl.innerHTML = `<p class="error-text">${err}.City not found</p>`;
+//       searchFormEl.reset();
+//       weatherDisplayEl.innerHTML = "";
+//       loaderEl.classList.add('is-hidden');
 //     });
 // };
+
+// const onSearchFormSubmit = (event) => {
+//   event.preventDefault();
+//   loaderEl.classList.remove('is-hidden');
+
+//   const city = event.currentTarget.elements.weather_input.value.trim();
+//   if (city === "") {
+//     errorDisplayEl.innerHTML = '<p class="error-text">Please write the name of the city</p>'
+//     loaderEl.classList.add('is-hidden');
+//     return;
+//   }
+//   errorDisplayEl.innerHTML = '';
+//   fetchWeatherData(city);
+// };
+
 // searchFormEl.addEventListener("submit", onSearchFormSubmit);
 
-
-
-
-
-
+// document.addEventListener("DOMContentLoaded", fetchWeatherData("Kyiv"));
